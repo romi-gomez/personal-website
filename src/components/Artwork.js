5663b38073085342fc8a706b7d02ee2835e39b29
Romi Gomez <contact@romigomez.com.ar>
2024-02-15 12:47:00 +0100

    add temporary instructions to canvas

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import styled from 'styled-components'
 import Grid from '../assets/artwork/Grid'
 import JustSomeCircles from '../assets/artwork/JustSomeCircles'
@@ -9,10 +10,28 @@ import SoundMountains from '../assets/artwork/SoundMountains'
 
 const ArtworkContainer = styled.div`
     width: 100%;
-    height: 100%;`
+    height: 100%;
+    background: #1A1A1A ;
+`
+
+const ArtworkInstructions = styled.p`
+    position: absolute;
+    bottom: 10px;
+    left: 10px;
+    font-size: 1rem;
+    color: white;
+    background: rgba(0, 0, 0, 0.5);
+    padding: 5px 15px;
+    border-radius: 5px;
+`
 
 
 const Artwork = ({artwork}) => {
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const handleArtworkClick = () => {
+    setIsPlaying(!isPlaying)
+  }
 
   const selectArtworkToShow = (artwork) => {
     switch (artwork.title) {
@@ -33,6 +52,7 @@ const Artwork = ({artwork}) => {
 
   return (
     <ArtworkContainer>
+        <ArtworkInstructions $visibility={isPlaying}>Click on the canvas to play/pause</ArtworkInstructions>
         {selectArtworkToShow(artwork)}
     </ArtworkContainer>
   )
