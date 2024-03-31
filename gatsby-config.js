535fee6e985dcd9c53dc7d54a1b5d912edda2b33
Romi Gomez <contact@romigomez.com.ar>
2024-03-31 14:14:34 -0300

    gatsby-config and files cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,19 +10,20 @@ module.exports = {
     siteUrl: "https://romigomez.com",
     url: "https://romigomez.com"
    },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/assets/images/icon.png"
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", 
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/assets/images/icon.png"
+      }
+    }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/assets/images/"
+      },
+      __key: "images"
     }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", 
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/assets/images/"
-    },
-    __key: "images"
-  }
-]
+  ]
 };
